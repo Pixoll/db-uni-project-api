6c889541af25c8ac78bf891e71002e749911b8b0
Pixoll <pixoll.gamer@gmail.com>
2024-06-19 23:14:30 -0400

    Generate date instead

--- src/main/java/org/dbuniproject/api/db/structures/Sale.java
+++ src/main/java/org/dbuniproject/api/db/structures/Sale.java
@@ -19,13 +19,10 @@ public record Sale(
         @Nonnull String clientRut,
         @Nonnull ArrayList<ProductSale> productSales
 ) implements JSONEncodable, Validatable {
-    private static final Date MISSING_DATE = new Date(-1);
-    private static final int ONE_MINUTE = 60_000;
-
     public Sale(JSONObject json) throws ValidationException {
         this(
                 -1,
-                new Date(json.optLong("date", -1)),
+                new Date(),
                 json.optString("cashierRut"),
                 json.optString("clientRut"),
                 Util.jsonArrayToList(json.optJSONArray("productSales", new JSONArray()), ProductSale.class)
@@ -47,15 +44,6 @@ public record Sale(
 
     @Override
     public void validate(@NotNull String parentName) throws ValidationException {
-        if (this.date.equals(MISSING_DATE)) {
-            throw new ValidationException("date", "Date is empty.");
-        }
-
-        final Date now = new Date();
-        if (this.date.after(now) || this.date.before(new Date(now.getTime() - ONE_MINUTE))) {
-            throw new ValidationException("date", "Date is out of bounds.");
-        }
-
         if (this.cashierRut.isEmpty()) {
             throw new ValidationException("cashierRut", "Cashier rut is empty.");
         }
